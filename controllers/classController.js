6a9f3faadf359bfb59cee7a1cf3116c0c2809426
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2023-05-09 11:39:00 +0400

    Class create, update and delete

--- controllers/classController.js
+++ controllers/classController.js
@@ -1,5 +1,6 @@
 import Class from "../models/classModel.js";
 
+// Create class
 export const createClass = async (req, res) => {
   const { name } = req.body;
   try {
@@ -15,5 +16,42 @@ export const createClass = async (req, res) => {
     res.status(201).json(newClass);
   } catch (err) {
     res.status(500).json({ errror: err.message });
+  }
+};
+
+// Update class
+export const updateClass = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updatedClass = await Class.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updateClass) {
+      return res.status(404).json({ message: "Class not found" });
+    }
+
+    res.status(200).json(updatedClass);
+  } catch (err) {
+    res.status(500).json({ message: { error: err.message } });
+  }
+};
+
+// Delete class
+
+export const deleteClass = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedClass = await Class.findByIdAndDelete(id);
+
+    if (!deletedClass) {
+      return res.status(404).json({ message: "Class not found" });
+    }
+
+    res.status(200).json({ message: "Class successfully deleted" });
+  } catch (err) {
+    res.status(500).json({ message: { error: err.message } });
   }
 };
--- routes/classRoutes.js
+++ routes/classRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { createClass } from "../controllers/classController.js";
+import {
+  createClass,
+  deleteClass,
+  updateClass,
+} from "../controllers/classController.js";
 
 const router = express.Router();
 
 router.post("/", createClass);
+router.put("/:id", updateClass);
+router.delete("/:id", deleteClass);
 
 export default router;
